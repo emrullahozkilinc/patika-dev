17f797b0f7e1eb326e2efdad1de0b5e8aadd0282
emrullahozkilinc <33889029+emrullahozkilinc@users.noreply.github.com>
2021-07-18 18:18:23 +0300

    like-ilike ifadeleri

---
+++ sql_odev_3.sql
@@ -0,0 +1,23 @@
+select country
+from country
+where country 
+	like 'A%a';
+
+select country
+from country
+where country 
+	like '_____%n';
+
+select title
+from film 
+where title
+	ilike '%t%t%t%t%';
+	
+select *
+from film
+where title 
+	like 'C%'
+	and
+	length > 90
+	and
+	rental_rate = 2.99;
