612690f5898a98be2752bf6a7ebc6b3581b15f43
emrullahozkilinc <33889029+emrullahozkilinc@users.noreply.github.com>
2021-07-22 23:16:19 +0300

    alter, delete, drop, create, update işlemleri.

---
+++ sql_odev_8.sql
@@ -0,0 +1,102 @@
+drop table if exists employee;
+
+create table employee(
+	id serial primary key,
+	name varchar(50),
+	birthdate date,
+	email varchar(100)
+);
+
+insert into employee (name, birthdate, email) values ('Dela Burnhill', '1956-02-02', 'dburnhill0@devhub.com');
+insert into employee (name, birthdate, email) values ('Sela Tooth', '1975-09-16', null);
+insert into employee (name, birthdate, email) values ('Merill Keemar', '2000-02-02', 'mkeemar2@1und1.de');
+insert into employee (name, birthdate, email) values ('Amye Balf', '1984-04-10', null);
+insert into employee (name, birthdate, email) values ('Chiquita Dulton', '1997-03-16', 'cdulton4@123-reg.co.uk');
+insert into employee (name, birthdate, email) values ('Ezra Gallamore', '1906-03-18', null);
+insert into employee (name, birthdate, email) values ('Merry Egle', '1938-06-16', 'megle6@msu.edu');
+insert into employee (name, birthdate, email) values ('Camile Fenlon', '1998-01-12', 'cfenlon7@who.int');
+insert into employee (name, birthdate, email) values ('Roderick Dobrowolski', '1927-07-06', 'rdobrowolski8@sakura.ne.jp');
+insert into employee (name, birthdate, email) values ('Karalynn Gurley', '1931-11-16', 'kgurley9@lycos.com');
+insert into employee (name, birthdate, email) values ('Micaela Belch', '1981-10-25', 'mbelcha@wikispaces.com');
+insert into employee (name, birthdate, email) values ('Arel Pea', '1907-10-17', 'apeab@odnoklassniki.ru');
+insert into employee (name, birthdate, email) values ('Alaine Goldstone', '1962-11-26', 'agoldstonec@fotki.com');
+insert into employee (name, birthdate, email) values ('Orelee Eglise', '1916-01-17', 'oeglised@forbes.com');
+insert into employee (name, birthdate, email) values ('Phyllida Scambler', '1910-06-24', 'pscamblere@dailymotion.com');
+insert into employee (name, birthdate, email) values ('Joann Simmers', '1930-01-05', 'jsimmersf@about.com');
+insert into employee (name, birthdate, email) values ('Derward Chishull', '1945-07-21', 'dchishullg@51.la');
+insert into employee (name, birthdate, email) values ('Ruthann Gloyens', '1902-11-29', 'rgloyensh@china.com.cn');
+insert into employee (name, birthdate, email) values ('Penni Walhedd', '1963-09-05', 'pwalheddi@spotify.com');
+insert into employee (name, birthdate, email) values ('Merla Melpuss', null, 'mmelpussj@twitter.com');
+insert into employee (name, birthdate, email) values ('Edmund Mellonby', '1904-10-04', 'emellonbyk@craigslist.org');
+insert into employee (name, birthdate, email) values ('Oralie Malam', '1982-01-15', 'omalaml@newyorker.com');
+insert into employee (name, birthdate, email) values ('Garrot Speek', '1965-10-01', 'gspeekm@360.cn');
+insert into employee (name, birthdate, email) values ('Lurline Gonoude', '1922-03-15', 'lgonouden@ftc.gov');
+insert into employee (name, birthdate, email) values ('Colman Karpychev', '1971-05-06', 'ckarpychevo@list-manage.com');
+insert into employee (name, birthdate, email) values ('Ayn Dimbleby', null, 'adimblebyp@newsvine.com');
+insert into employee (name, birthdate, email) values ('Hermione Hainsworth', null, 'hhainsworthq@hexun.com');
+insert into employee (name, birthdate, email) values ('Rosemonde Kinge', '1960-11-07', 'rkinger@upenn.edu');
+insert into employee (name, birthdate, email) values ('Rafaello Brotherhead', '1924-01-24', 'rbrotherheads@rambler.ru');
+insert into employee (name, birthdate, email) values ('Cloe Blatherwick', '1993-12-13', 'cblatherwickt@liveinternet.ru');
+insert into employee (name, birthdate, email) values ('Naomi MacCoughan', '1962-03-21', 'nmaccoughanu@oracle.com');
+insert into employee (name, birthdate, email) values ('Shannen Newlan', '1933-11-22', 'snewlanv@harvard.edu');
+insert into employee (name, birthdate, email) values ('Willey Biasioli', '1955-05-11', 'wbiasioliw@studiopress.com');
+insert into employee (name, birthdate, email) values ('Gerda Osmint', '1973-06-10', 'gosmintx@domainmarket.com');
+insert into employee (name, birthdate, email) values ('Leora Giacopini', '1986-10-02', 'lgiacopiniy@addtoany.com');
+insert into employee (name, birthdate, email) values ('Griselda Trevna', '1982-05-28', 'gtrevnaz@upenn.edu');
+insert into employee (name, birthdate, email) values ('Joli Shinton', '1977-06-08', 'jshinton10@reverbnation.com');
+insert into employee (name, birthdate, email) values ('Babette Taile', '1986-12-19', 'btaile11@vk.com');
+insert into employee (name, birthdate, email) values ('Farr Tremayle', '1912-12-31', 'ftremayle12@wikipedia.org');
+insert into employee (name, birthdate, email) values ('Benito Scarffe', '1988-09-13', 'bscarffe13@ask.com');
+insert into employee (name, birthdate, email) values ('Terrel Pavek', '1961-09-21', 'tpavek14@nydailynews.com');
+insert into employee (name, birthdate, email) values ('Bob Poynser', '1968-01-19', 'bpoynser15@va.gov');
+insert into employee (name, birthdate, email) values ('Faber Campa', '1994-08-25', 'fcampa16@topsy.com');
+insert into employee (name, birthdate, email) values ('Aggi Kobu', '1944-01-14', 'akobu17@ed.gov');
+insert into employee (name, birthdate, email) values ('Dorene Varsey', '1940-04-08', 'dvarsey18@craigslist.org');
+insert into employee (name, birthdate, email) values ('Linell Giles', '1918-07-16', 'lgiles19@weibo.com');
+insert into employee (name, birthdate, email) values ('Daniella Brayson', '1952-03-18', null);
+insert into employee (name, birthdate, email) values ('Claudia Huggins', '1966-03-10', 'chuggins1b@addtoany.com');
+insert into employee (name, birthdate, email) values ('Rodrique MacKibbon', '1948-06-10', 'rmackibbon1c@ihg.com');
+insert into employee (name, birthdate, email) values ('Janella Shieldon', '1958-12-31', 'jshieldon1d@prweb.com');
+
+update employee
+set name = 'empty'
+where email like 'f%'
+returning *;
+
+update employee
+set birthdate = '1920-01-01'
+where birthdate < '1920-01-01'
+returning *;
+
+update employee
+set name = 'emrullah özkılınç',
+	birthdate = '1994-11-15',
+	email = 'emr.ozk.71@gmail.com'
+where id = '25'
+returning *;
+
+update employee
+set name = 'ali yılmaz'
+where email like '%google%';
+
+update employee
+set email = 'wrong mail'
+where birthdate between '1990-01-01' and '1985-01-01';
+
+delete employee
+where birthdate 
+	between '1920-01-01' and '1925-01-01';
+
+delete employee
+where name like '%ç%' or 
+	  name like '%ş%' or 
+	  name like '%ğ%' or 
+	  name like '%ü%' or 
+	  name like '%ı%';
+
+delete employee
+where email like '%@about.___';
+
+delete employee
+where name != 'Garrot' or
+	  birthdate not between '1900-01-01' and '1990-01-01';
