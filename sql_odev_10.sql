969d9e43898869fe4ed3d6654000197b99111f21
emrullahozkilinc <33889029+emrullahozkilinc@users.noreply.github.com>
2021-07-23 22:40:05 +0300

    left, right ve full join işlemleri.

---
+++ sql_odev_10.sql
@@ -0,0 +1,11 @@
+select city.city, country.country
+from city
+left join country on city.country_id = country.country_id;
+
+select payment_id, first_name, last_name
+from customer
+right join payment on payment.customer_id = customer.customer_id;
+
+select rental_id, first_name, last_name
+from customer
+full join rental on rental.customer_id = customer.customer_id;
