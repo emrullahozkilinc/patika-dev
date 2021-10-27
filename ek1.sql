a7239207da115457d66ac8333378f062676473d3
Emrullah Özkılınç <emr.ozk.71@gmail.com>
2021-10-27 20:02:16 +0300

    Analytic Funcs added

--- ek1.sql
+++ ek1.sql
@@ -1,3 +1,4 @@
+-- 1
 with dq as(
 select date(payment_date) as pd,
 	count(*) as toplam_satis
@@ -13,3 +14,13 @@ select *,
 		rows between 1 preceding and current row
 		) as cum_sum
 from dq
+
+
+-- 2
+select *,
+	first_value(payment_id)
+		over(
+			partition by customer_id
+			rows between unbounded preceding and unbounded following
+		) as first_payment
+from payment
