972d1f2783bd6b8774eddbbbe93ae4eb1828bb2c
Emrullah <emr.ozk.71@gmail.com>
2022-03-22 22:26:24 +0300

    added for loop

--- src/pac1/Main1.java
+++ src/pac1/Main1.java
@@ -2,5 +2,8 @@ package pac1;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Hello, World!");
+        }
     }
 }
